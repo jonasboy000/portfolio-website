f45ae39bd9ca252e71c46816fd9f4459be6a9f3a
Jonas Deschaek <jonasdeschaek000@gmail.com>
2026-05-04 11:55:12 +0200

    fixed views path for local and production environments

--- index.ts
+++ index.ts
@@ -6,6 +6,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const year = new Date().getFullYear();
 
+const viewsPath = path.join(
+  __dirname,
+  process.env.NODE_ENV === "production" ? "../views" : "views",
+);
+const publicPath = path.join(
+  __dirname,
+  process.env.NODE_ENV === "production" ? "../public" : "public",
+);
+
+app.set("views", viewsPath);
+app.use(express.static(publicPath));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 app.use(express.static(path.join(__dirname, "../public")));
